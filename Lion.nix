ff37e0273a73e3a9ca19ff45e02e4f5786ee72a5
maeve <maeve@oa.ke>
2025-05-01 09:35:52 +0100

    update nix file

--- Lion.nix
+++ Lion.nix
@@ -1,14 +1,18 @@
-{ stdenv, lib, fetchFromGitHub }:
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+}:
 
 stdenv.mkDerivation rec {
   name = "Lion-theme";
-  version = "1.1";
+  version = "1.2";
 
   src = fetchFromGitHub {
     owner = "maeve-oake";
     repo = "Lion";
-    rev = "360384c6f4806b84e2c593edb27d240a6b824789";
-    sha256 = "sha256-n6r5oOuxjY86TLD4YM51rENHUR4qfzvlf4D3hZHpkyM=";
+    rev = "2b790684f727f893fa2680e4144d5df4187cc0d3";
+    sha256 = "sha256-AQgTzU6DRWOBch79gyuvhrYv0ZkUvZQYn1UdNRdNmkE=";
   };
 
   installPhase = ''
